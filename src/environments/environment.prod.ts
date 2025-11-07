2415ed4d1c28441def5158e6b18b272cbe504876
AmrTarek7 <amryousef04@gmail.com>
2025-11-07 20:44:48 +0200

    chore: update environment configuration for production and development
    - Set production flag to true in environment.prod.ts.
    - Set production flag to false in environment.ts for development.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 import { Languages } from '../app/core/enums/languages';
 
 export const environment = {
+  production: true,
   state: 'production',
   defaultLanguage: Languages.AR,
   HOST_API: 'http://luxoruniversityapi.runasp.net/api/'
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 import { Languages } from '../app/core/enums/languages';
 
 export const environment = {
+  production: false,
   state: 'development',
   defaultLanguage: Languages.AR,
   HOST_API: 'http://luxoruniversityapi.runasp.net/api/'
